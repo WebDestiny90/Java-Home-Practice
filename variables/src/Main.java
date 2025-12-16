52f198d0f3efbcb47e9e0a2ad153b8952060bd12
WebDestiny90 <azeagent@gmail.com>
2025-12-16 19:36:44 +0400

    Java Variables Practice

---
+++ variables/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+  public static void main(String[] args) {
+    byte bte = 1;
+    System.out.println(bte);
+    short shrt = 5;
+    System.out.println(shrt);
+    int i = 100;
+    System.out.println(i);
+    long lng = 1000L;
+    System.out.println(lng);
+    float flt = 1.5F;
+    System.out.println(flt);
+    double dbl = 5.5;
+    System.out.println(dbl);
+    boolean bln = true;
+    System.out.println(bln);
+    char chr = '#';
+    System.out.println(chr);
+    String str = "String";
+    System.out.println(str);
+  }
+}
